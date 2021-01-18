4a2b4ac9db1db130f1a53cbdcf00ff899a091047
Evgeniy Khodzitskiy <m1ndcoderr@gmail.com>
2021-01-18 16:03:51 +0300

    test (heroku)

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const port = process.env.APP_PORT
+  const port = process.env.PORT || 3333
 
   const app = await NestFactory.create(AppModule)
   await app.listen(port, () => console.log('Server is running on port ' + port + '...'))
